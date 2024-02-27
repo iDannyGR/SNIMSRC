578af679366112275bf13203ed5370a8dc320665
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2024-02-26 20:57:20 -0600

    update

---
+++ backend/src/Organization/organization.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Put, Post, Delete, Body, Param } from "@nestjs/common";
+import { OrganizationService } from "./organization.service";
+import {Organization} from '@prisma/client'\;
+
+@Controller('organization')
+export class orgnizationController{
+   constructor(private readonly organizationService: OrganizationService){}
+
+
+   @Get()
+   async getOrganization(){
+    this.organizationService.getOrganization()
+   }
+
+   @Post()
+   async createOrganization(@Body data:Organization){
+        this.organizationService.createOrganization(data);
+   }
+   
+   @Delete(':id')
+   async deleteOrganization(@Param('id') id:string){
+      this.organizationService.deleteOrganization(Number(id))
+   }
+}
---
+++ backend/src/Organization/organization.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@nestjs/common";
+import { Organization } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class OrganizationService {
+  constructor(private prisma: PrismaService) {}
+
+  async getOrganization(): Promise<Organization[]> {
+    return this.prisma.organization.findMany();
+  }
+
+  async createOrganization(data: Organization): Promise<Organization> {
+    return this.prisma.organization.create({ data });
+  }
+
+  async updateOrganization(id: number, data: Organization): Promise<Organization> {
+    return this.prisma.organization.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async deleteOrganization(id: number): Promise<Organization> {
+    return this.prisma.organization.delete({
+      where: { id }
+    });
+  }
+}
