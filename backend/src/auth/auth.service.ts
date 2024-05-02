b21f5651d54d80715175310e6004e506987ac9c2
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2024-05-02 15:37:27 -0600

    upadte service auth

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -8,27 +8,37 @@ import { excludeFromObject, excludeFromList } from 'src/utils/excludeFields';
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async create(UserData: RegisterDto): Promise<RegisterDto> {
-    const { password } = UserData;
-    return await this.prisma.user.create({
-      data: {
-        ...UserData,
-        password: encrptPsw(password),
-      },
-    });
+  async create(UserData: RegisterDto): Promise<GetUserDto> {
+    try {
+
+      const { password } = UserData;
+      const encryptPassword = encrptPsw(password) ;
+
+      const newUser= await this.prisma.user.create({
+        data: {
+          ...UserData,
+          password: encryptPassword,
+        },
+      });
+
+      const filterData = excludeFromObject(newUser, ['password']);
+      return filterData;
+
+    } catch (error) {
+      throw new Error(error);
+    }
+    
   }
 
   async findAll(): Promise<GetUserDto[]> {
-      try {
-        const data = await this.prisma.user.findMany();
-        if (!data || data.length === 0) {
-          return [];
-        }
-        const filterData = excludeFromList(data, ['password']);
-        return filterData;
-      } catch (error) {
-    throw new Error('No se pudieron recuperar usuarios después de varios intentos');
-        }
+    try {
+      const data = await this.prisma.user.findMany();
+      if (!data || data.length === 0) throw new NotFoundException('error retriving information');
+      const filterData = excludeFromList(data, ['password']);
+      return filterData;
+    } catch (error) {
+      throw new Error('bad data request');
+    }
   }
 
   async findOne(id: number): Promise<GetUserDto> {
