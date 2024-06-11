9632a03299b6ffb2ab43fde820beda6cc6055f26
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2024-06-11 15:55:53 -0600

    update auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,20 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    PrismaModule,
+    UsersModule,
     JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('secretkey'),
+      useFactory: () => ({
         signOptions: { expiresIn: '60m' },
         global: true,
       }),
