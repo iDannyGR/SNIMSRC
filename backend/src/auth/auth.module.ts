271d56016155b526345b5dd74f15cd93e536f1f1
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2024-07-29 20:57:02 -0600

    add asincronous jwt

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,16 +3,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.MYSECRET_KEY,
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('secretkey'),
+        signOptions: { expiresIn: '60m' },
+        global: true,
+      }),
     }),
   ],
 })
