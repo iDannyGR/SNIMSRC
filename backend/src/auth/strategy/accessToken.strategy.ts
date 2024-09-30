1252faf39256c8895f8bc1824f84bb9c3b6bc65d
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2024-09-30 15:48:18 -0600

    update strategy

--- backend/src/auth/strategy/accessToken.strategy.ts
+++ backend/src/auth/strategy/accessToken.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 type JwtPayload = {
   sub: string;
@@ -9,10 +10,10 @@ type JwtPayload = {
 
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_ACCESS_SECRET,
+      secretOrKey: configService.get<string>('secretkey'),
     });
   }
 
