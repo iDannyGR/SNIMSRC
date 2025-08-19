9df8d13c9e8e92bc168ee606caa90a57d4fb6198
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2025-08-19 15:59:34 -0600

    rollback frontend

--- frontend/.prettierrc.js
+++ frontend/.prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  trailingComma: "none",
-  tabWidth: 2,
-  semi: true,
-  bracketSpacing: true,
-  printWidth: 105,
-  endOfLine: "auto",
-  singleQuote: false,
-  useTabs: false
+	trailingComma: 'none',
+	tabWidth: 2,
+	semi: true,
+	bracketSpacing: true,
+	printWidth: 105,
+	endOfLine: 'auto',
+	singleQuote: false,
+	useTabs: false,
 };
---
+++ frontend/src/hooks/useCreateNote.ts
@@ -0,0 +1,36 @@
+import { useFormik } from 'formik';
+import { toast } from 'react-toastify';
+import { noteSchema } from '@/schemas/note.schema';
+// import { httpClient } from "@/lib/api/axios.config";
+
+interface noteData {
+	date: Date; // Cambiado a Date o string para manejar fechas,
+	site: string;
+	object: string;
+	description: string;
+	results: string;
+}
+
+const initialValues: noteData = {
+	date: new Date(), // Inicializa con la fecha actual
+	site: '',
+	object: '',
+	description: '',
+	results: '',
+};
+
+const useCreateNote = () => {
+	const formik = useFormik({
+		enableReinitialize: true,
+		initialValues: initialValues,
+		validationSchema: noteSchema,
+		validateOnBlur: true,
+		validateOnChange: true,
+		onSubmit: async (values) => {
+			toast.success('Note created successfully!');
+			console.log('Submitting note:', values);
+		},
+	});
+	return formik;
+};
+export { useCreateNote };
