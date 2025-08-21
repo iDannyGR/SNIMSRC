0ce06c4e9f2adc29025569ac355ba4ad342bdab8
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2025-08-20 21:26:15 -0600

    update config

--- frontend/src/api/axios.config.ts
+++ frontend/src/api/axios.config.ts
@@ -1,34 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_BACKEND || 'http://localhost:3000/api/v1',
-	headers: {
-		'Content-Type': 'application/json',
-		Accept: 'application/json',
-	},
+  baseURL: process.env.NEXT_PUBLIC_BACKEND || "http://localhost:3000/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  }
 });
 
 axiosInstance.interceptors.request.use(
-	(config) => {
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 axiosInstance.interceptors.response.use(
-	(response) => response,
-	(error) => {
-		// Manejo centralizado de errores
-		return Promise.reject(error);
-	}
+  (response) => response,
+  (error) => {
+    // Manejo centralizado de errores
+    return Promise.reject(error);
+  }
 );
 
 export const httpClient = {
-	get: <T>(url: string, params?: object) => axiosInstance.get<T>(url, { params }),
-	post: <T>(url: string, data: object, config?: object) => axiosInstance.post<T>(url, data, config),
-	put: <T>(url: string, data: object) => axiosInstance.put<T>(url, data),
-	patch: <T>(url: string, data: object) => axiosInstance.patch<T>(url, data),
-	delete: <T>(url: string) => axiosInstance.delete<T>(url),
+  get: <T>(url: string, params?: object) => axiosInstance.get<T>(url, { params }),
+  post: <T>(url: string, data: object, config?: object) => axiosInstance.post<T>(url, data, config),
+  put: <T>(url: string, data: object) => axiosInstance.put<T>(url, data),
+  patch: <T>(url: string, data: object) => axiosInstance.patch<T>(url, data),
+  delete: <T>(url: string) => axiosInstance.delete<T>(url)
 };
