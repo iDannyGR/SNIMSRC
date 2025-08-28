df5e264e8c905dd1d078f7dd7a2ce4db36db3a5e
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2025-08-28 17:11:20 -0600

    update

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function SideBar(): React.JSX.Element {
-  return <div className="w-[250px] h-[100vh] shadow-md"></div>;
+  return <div className="w-64 h-[100vh] bg-blue-950">
+    
+  </div>;
 }
