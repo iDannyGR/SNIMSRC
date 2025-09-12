17c4cdb8743423d213adaf801cd54eece9008c53
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2025-09-12 15:57:57 -0600

    update modal

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
-import Modal from "@/components/Modal/Modal";
 import React from "react";
+import Modal from "@/components/Modal/Modal";
+import NoteForm from "@/components/NoteForm";
 import { modalStore } from "@/store/modalStore";
 export default function Page() {
   const { isOpen, closeModal } = modalStore();
   return (
     <div>
       <Modal isOpen={isOpen} onClose={closeModal}>
-        body
+      <NoteForm />
       </Modal>
     </div>
   );
--- frontend/src/components/noteForm.tsx
+++ frontend/src/components/noteForm.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { Textarea } from "@headlessui/react";
+import React from "react";
 
-export default function noteForm() {
+export default function NoteForm(): React.JSX.Element {
   return (
     <div>
-        <form action="">
-            
-        </form>
+      <form action="">
+        <Textarea name="objetivo" className="border"></Textarea>
+      </form>
     </div>
-  )
+  );
 }
